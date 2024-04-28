22ea1462cac6b30f00f1a956b794b8e4b44e2c6e
Szymon Gutaj <bumbosiepsak@googlemail.com>
2024-04-28 18:14:08 +0200

    Opera Software Recruitment Coding Task - Adding the "run.sh" script intended for running the project (Docker services) - Adding the "stop.sh" script intended for stopping the project (Docker services) - Adding the "download_model.sh" script intended for downloading LLM models

---
+++ download_model.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
+
+export PROJECT_ROOT="$(readlink -e $(dirname "${0}"))"
+
+source "${PROJECT_ROOT}/source/glue/runners.sh"
+
+validate_provisioning
+
+# NOTE: Substantial logic hardcoded here for the sake of demo simplicity
+if [[ "${1:-not_full}" == "full" ]]; then
+    echo >&2 "INFO: You're downloading a full demo with the 'HuggingFaceH4/starchat-beta' model (59GB)"
+
+    LLM_MODEL_NAME="HuggingFaceH4/starchat-beta"
+    LLM_MODEL_VERSION="b1bcda690655777373f57ea6614eb095ec2c886f"
+    LLM_MODEL_URI="https://huggingface.co/HuggingFaceH4/starchat-beta"
+    IS_GATED_MODEL=0
+
+elif [[ "${1:-not_small}" == "small" ]]; then
+    echo >&2 "INFO: You're downloading a smaller demo with the 'google-t5/t5-small' model (2GB)"
+
+    LLM_MODEL_NAME="google-t5/t5-small"
+    LLM_MODEL_VERSION="df1b051c49625cf57a3d0d8d3863ed4d13564fe4"
+    LLM_MODEL_URI="https://huggingface.co/google-t5/t5-small"
+    IS_GATED_MODEL=0
+
+else
+    echo >&2 "ERROR: Expecting a model to be chosen for download"
+    echo >&2 "INFO: Please type: 'download_model.sh full' for a full, 59GB download (HuggingFaceH4/starchat-beta)"
+    echo >&2 "INFO: Please type: 'download_model.sh small' for a smaller, 2GB download (google-t5/t5-small)"
+
+    exit 1
+fi
+
+if ((${IS_GATED_MODEL})); then
+    echo dups ${IS_GATED_MODEL}
+    validate_gated_model_access "${LLM_MODEL_NAME}" "${LLM_MODEL_URI}"
+
+    export HF_HUB_ENABLE_HF_TRANSFER="${HF_TOKEN}" # NOTE: Feed "text-generation-inference" too
+else
+    # NOTE: No special access rights needed
+    export HF_TOKEN=
+    export HF_HUB_ENABLE_HF_TRANSFER=
+fi
+
+run_command \
+    --entrypoint /download_model.sh \
+    --env HF_HUB_ENABLE_HF_TRANSFER=1 \
+    --env HF_TOKEN \
+    --interactive \
+    coding_assistant \
+        "${LLM_MODEL_NAME}" \
+        "${LLM_MODEL_VERSION}"
---
+++ source/glue/runners.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo >&2 "ERROR: This file is supposed to be sourced!"
+    exit 1
+fi
+
+PROJECT_COMPOSE_FILE="source/containers/docker-compose.yaml"
+PROJECT_NAME="coding_assistant"
+
+function validate_provisioning() {
+    if ! command -v docker > /dev/null 2>&1; then
+        echo >&2 "ERROR: 'docker' not installed. Please install e.g. with the 'provision_linux_x86_64.sh' script"
+        exit 1
+    fi
+
+    if ! command -v docker compose > /dev/null 2>&1; then
+        echo >&2 "ERROR: 'docker compose' not installed. Please install e.g. with the 'provision_linux_x86_64.sh' script"
+        exit 1
+    fi
+
+    if ! docker buildx > /dev/null 2>&1; then
+        echo >&2 "ERROR: 'docker buildx' not installed. Please install e.g. with the 'provision_linux_x86_64.sh' script"
+        exit 1
+    fi
+}
+
+function validate_gated_model_access() {
+    local MODEL_NAME="${1}"
+    local MODEL_URI="${2}"
+
+    if [[ "${HF_TOKEN:-not_set}" == "not_set" ]]; then
+        echo >&2 "ERROR: No HuggingFace access token present in the HF_TOKEN environment variable"
+        echo >&2 "INFO: ${MODEL_NAME} is a gated model and requires gaining access rights prior to download"
+        echo >&2 "INFO: 1. Please obtain your personal access key from: https://huggingface.co/settings/tokens"
+        echo >&2 "INFO: 2. Then pass it via the HF_TOKEN environment variable, e.g. by setting: 'export HF_TOKEN=<token here>'"
+        echo >&2 "INFO: 3. Then ask for access in: ${MODEL_URI}"
+        echo >&2 "INFO: 4. Once access is granted, then run 'download_model.sh' again (within the shell from point 2)"
+
+        exit 1
+    fi
+}
+
+function run_server() {
+    local PROFILE="${1}"
+
+    export USER_UID="${USER_UID:-"$(id --user)"}"
+    export USER_GID="${USER_GID:-"$(id --group)"}"
+
+    mkdir --parents "${PROJECT_ROOT}/volumes/models"
+
+    docker compose \
+        --env-file "${PROJECT_ROOT}/source/containers/docker-compose-service.env" \
+        --file "${PROJECT_ROOT}/${PROJECT_COMPOSE_FILE}" \
+        --profile "${PROFILE}" \
+        --project-directory "${PROJECT_ROOT}" \
+        --project-name "${PROJECT_NAME}" \
+        up \
+            --build \
+            --detach \
+            --force-recreate \
+            --remove-orphans
+}
+
+function stop_server() {
+    docker compose \
+        --env-file "${PROJECT_ROOT}/source/containers/docker-compose-service.env" \
+        --file "${PROJECT_ROOT}/${PROJECT_COMPOSE_FILE}" \
+        --project-directory "${PROJECT_ROOT}" \
+        --project-name "${PROJECT_NAME}" \
+        down \
+            --remove-orphans
+}
+
+function run_command() {
+    local ARGUMENTS="${@}"
+
+    docker compose \
+        --env-file "${PROJECT_ROOT}/source/containers/docker-compose-service.env" \
+        --file "${PROJECT_ROOT}/${PROJECT_COMPOSE_FILE}" \
+        --project-directory "${PROJECT_ROOT}" \
+        --project-name "${PROJECT_NAME}" \
+        --rm \
+        run \
+            "${@}"
+}
